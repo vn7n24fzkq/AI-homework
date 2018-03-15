ad7dd9556e56110f6a9ace5c68e91972b075c9bf
VN7 <vn7n24fzkq@gmail.com>
2018-03-16 01:01:18 +0800

    add PerceptronTraining predict function

--- PerceptronTraining/PerceptronTraining.rs
+++ PerceptronTraining/PerceptronTraining.rs
@@ -1,4 +1,5 @@
 use std::f64::consts::E;
+use std::io;
 
 fn main(){
 
@@ -11,35 +12,20 @@ fn main(){
     let mut w = def_w.clone();
     let mut avg_err = 0_f64;
 
-    for _i in 0..1000{
+    println!("Enter training times");
+    let mut input_times = String::new();
+    io::stdin()
+        .read_line(&mut input_times)
+        .expect("failed to read from stdin");
+
+    let times: i32= input_times.trim().parse().unwrap();
+
+    for _i in 0..times{
         println!("This is {} generation",_i+1);
         println!("{0:<3} | {1:<7} || {2:<7} | {3:<7} | {4:<7} | {5:<7} | {6:<7} || {7:<7} | {8:<7} | {9:<7} | {10:<7}",
                                                                             "x", "f(x)", "in", "si(in)", "si(in)'", "error", "d_wi", "t", "w1", "w2","w3");
         for j in 0.._x.len(){
-
-            let mut _in: f64 = 0.0;
-
-            for k in 0.._x[j].len(){
-
-                _in += (_x[j][k] as f64) * w[k];
-            }
-
-            _in += -t;
-
-            let _out: f64 = sigmoid(_in);
-            let _err: f64 = _answer[j] as f64 - _out;
-            let mut delta_w: f64 = _a *  _err * (_out * (1.0 - _out));
-
-            avg_err += _err.powf(2.0);
-
-            for z in 0..w.len(){
-
-                w[z] += delta_w * _x[j][z] as f64;
-            }
-
-            t -= delta_w;
-
-            println!("{0:<03.2} | {1:<7.3} || {2:<7.3} | {3:<7.3} | {4:<7.3} | {5:<7.3} | {6:<7.3} || {7:<7.3} | {8:<7.3} | {9:<7.3} | {10:<7.3}", _x[j][0]*100+_x[j][1]*10+_x[j][2], _answer[j], _in, _out, (_out * (1.0 - _out)), _err, delta_w, t, w[0], w[1],w[2]);
+            avg_err += train(_x[j],_answer[j],_a,&mut t,&mut w);         
         }
         
     avg_err /=  _x.len() as f64;
@@ -50,6 +36,51 @@ fn main(){
     def_w = w.clone();
     avg_err = 0.0;
     }
+
+    let mut input_x = String::new();
+
+    println!("Enter test data. e.g. 110");
+    io::stdin()
+        .read_line(&mut input_x)
+        .expect("failed to read from stdin");
+    let test_data: i32 = input_x.trim().parse().unwrap();
+    let input_arr = [test_data % 100, test_data % 10 ,test_data % 1];
+    println!("x is {}{}{} , sigma(in) is {:.3}",input_arr[0],input_arr[1],input_arr[2],predict(input_arr,&mut t,&mut w));
+}
+
+fn train(input: [i32; 3],ans: i32,_a: f64,t: &mut f64,w: &mut [f64; 3]) -> f64{
+    let mut _in: f64 = 0.0;
+
+    for i in 0..input.len(){
+        _in += (input[i] as f64) * &w[i];
+    }
+
+    _in -= *t;
+
+    let _out: f64 = sigmoid(_in);
+    let _err: f64 = ans as f64 - _out;
+    let delta_w: f64 = _a * _err * (_out * (1.0 - _out));
+
+    for j in 0..w.len(){
+        w[j] += delta_w * input[j] as f64;
+    }
+
+    *t -= delta_w;
+
+    println!("{0:<03.2} | {1:<7.3} || {2:<7.3} | {3:<7.3} | {4:<7.3} | {5:<7.3} | {6:<7.3} || {7:<7.3} | {8:<7.3} | {9:<7.3} | {10:<7.3}", input[0]*100+input[1]*10+input[2], ans, _in, _out, (_out * (1.0 - _out)), _err, delta_w, t, w[0], w[1], w[2]);
+    return _err.powf(2.0);
+}
+
+fn predict(input: [i32; 3],t: &mut f64,w: &mut [f64; 3]) -> f64{
+    let mut _in: f64 = 0.0;
+
+    for i in 0..input.len(){
+        _in += (input[i] as f64) * &w[i];
+    }
+
+    _in -= *t;
+
+    return sigmoid(_in);
 }
 
 fn sigmoid(x: f64) -> f64{
